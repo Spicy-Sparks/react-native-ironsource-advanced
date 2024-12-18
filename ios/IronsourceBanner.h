8ecc40026bed6bbbbfb8661e23215402cfc93975
Alessandro Gerelli <geroale2000@gmail.com>
2024-12-18 17:18:15 +0100

    Fix

--- ios/IronsourceBanner.h
+++ ios/IronsourceBanner.h
@@ -1,15 +1,8 @@
 #import <IronSource/IronSource.h>
 #import <React/RCTEventEmitter.h>
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNIronsourceAdvancedSpec.h"
-
-@interface IronsourceBanner : RCTEventEmitter <NativeIronsourceAdvancedSpec, LevelPlayBannerDelegate>
-#else
 #import <React/RCTBridgeModule.h>
 
-@interface IronsourceBanner : RCTEventEmitter <RCTBridgeModule, LevelPlayBannerDelegate>
-#endif
+@interface IronsourceBanner : RCTEventEmitter <RCTBridgeModule, LPMBannerAdViewDelegate>
 
 @property (class, nonatomic, strong) ISBannerView *bannerView;
 
--- ios/IronsourceSegment.h
+++ ios/IronsourceSegment.h
@@ -1,13 +1,6 @@
 #import <React/RCTEventEmitter.h>
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNIronsourceAdvancedSpec.h"
-
-@interface IronsourceSegment : NSObject <NativeIronsourceAdvancedSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface IronsourceSegment : NSObject <RCTBridgeModule>
-#endif
 
 @end
