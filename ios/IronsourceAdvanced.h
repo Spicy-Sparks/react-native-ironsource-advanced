83ef1f57f1c0965a1cd965950d6cc1af8de84a8d
Alessandro Gerelli <geroale2000@gmail.com>
2024-12-18 17:18:15 +0100

    fix:dropped incomplete new arch support

--- ios/IronsourceAdvanced.h
+++ ios/IronsourceAdvanced.h
@@ -1,14 +1,7 @@
 #import <IronSource/IronSource.h>
 #import <React/RCTEventEmitter.h>
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNIronsourceAdvancedSpec.h"
-
-@interface IronsourceAdvanced : RCTEventEmitter <NativeIronsourceAdvancedSpec, ISImpressionDataDelegate, ISInitializationDelegate>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface IronsourceAdvanced : RCTEventEmitter <RCTBridgeModule, ISImpressionDataDelegate, ISInitializationDelegate>
-#endif
 
 @end
--- ios/IronsourceRewarded.h
+++ ios/IronsourceRewarded.h
@@ -1,14 +1,7 @@
 #import <IronSource/IronSource.h>
 #import <React/RCTEventEmitter.h>
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNIronsourceAdvancedSpec.h"
-
-@interface IronsourceRewarded : RCTEventEmitter <NativeIronsourceAdvancedSpec, LevelPlayRewardedVideoDelegate>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface IronsourceRewarded : RCTEventEmitter <RCTBridgeModule, LevelPlayRewardedVideoDelegate>
-#endif
 
 @end
